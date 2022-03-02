62bc0dd779a1966135975d2c13434a0721236609
Vinit Bolla <vinitbolla10@gmail.com>
2022-03-03 04:19:29 +0530

    get API to get all posts

--- express.js
+++ express.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser");
 const compress = require("compression");
 const cors = require("cors");
 const helmet = require("helmet");
+const multer = require('multer');
+const post = require('./models');
 
 const app = express();
 
@@ -18,11 +20,40 @@ app.use(helmet());
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
+//View engiene
+app.set("view engine", "ejs");
+
 // Catch unauthorised errors
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: err.name + ": " + err.message });
   }
+});
+
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+
+var upload = multer({ storage: storage });
+
+// - the GET request handler that provides the HTML UI
+
+app.get('/', (req, res) => {
+  post.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('An error occurred', err);
+    }
+    else {
+      res.render('imagesPage', { items: items });
+    }
+  });
 });
 
 
